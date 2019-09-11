9695875d45218f2da4df7c5cd42e0473b2872381
Martin Sjåstad <m.sjastad@gmail.com>
2019-09-11 16:59:51 +0200

    Add max retries and handle when quote text is empty/undefined

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
 #! /usr/bin/env node
-'use strict';
+"use strict";
 
-const request = require('request');
+const r = require("request");
 
 const options = {
-  url: 'http://api.forismatic.com/api/1.0/',
-  method: 'POST',
+  url: "http://api.forismatic.com/api/1.0/",
+  method: "POST",
   form: {
-    method: 'getQuote',
-    lang: 'en',
-    format: 'json'
+    method: "getQuote",
+    lang: "en",
+    format: "json"
   },
   json: true
 };
 
-request(options, function(error, response, body) {
-  if (error) console.error(error);
-  if (response.statusCode === 200) {
-    console.log(body.quoteText, '--', body.quoteAuthor);
-  } else {
-    console.error('Error:', response.statusCode, ':', response.statusMessage);
+const MAX_RETRIES = 5;
+let retries = 0;
+
+function run() {
+  retries = retries + 1;
+  if (retries >= MAX_RETRIES) {
+    return console.log(`timeout after ${MAX_RETRIES}`);
   }
-});
+  return r(options, function(error, response, body) {
+    if (error) console.error(error);
+    if (response.statusCode === 200 && !!body.quoteText) {
+      return console.log(`${body.quoteText.trim()} -- ${body.quoteAuthor.trim()}`);
+    }
+    if (response.statusCode > 200) {
+      return console.error(
+        `Error: ${response.statusCode} - ${response.statusMessage}`
+      );
+    }
+    if (!body.quoteText || !body.quoteAuthor) {
+      run();
+    }
+  });
+}
+
+run();
